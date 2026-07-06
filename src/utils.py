de63b4d74b4e35a83bd6018a4b3dd1968e2a134b
Boni694 <02boniamin@gmail.com>
2026-07-06 17:01:10 +0600

    Add predict_pipeline.py

--- src/utils.py
+++ src/utils.py
@@ -68,14 +68,13 @@ def evaluate_models(X_train, y_train, X_test, y_test, models,params):
     except Exception as e:
         raise CustomException(e, sys)
     
-
-##        for model_name, model in models.items():
-##        y_test_pred = model.predict(X_test)    # Predict
-##           report[model_name] = test_model_score
-##       return report
-#
- #   except Exception as e:
-  #      raise CustomException(e, sys)
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+        
+    except Exception as e:
+        raise CustomException(e,sys)
 
 
 
